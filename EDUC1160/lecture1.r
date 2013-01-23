809208d78d22a8e7dbaa65fcc856c35ee4045ae2
jasonpbecker <jason.paul.becker@gmail.com>
2013-01-23 09:22:47 -0500

    First commit

---
+++ EDUC1160/lecture1.r
@@ -0,0 +1,37 @@
+---
+title       : EDUC1160: Evaluating the Impact of Social Programs 
+subtitle    : Causal Research Design
+author      : Jason Becker
+job         : Harvard Strategic Data Fellow, AISR/PPSD
+framework   : io2012        # {io2012, html5slides, shower, dzslides, ...}
+highlighter : prettify  # {highlight.js, prettify, highlight}
+hitheme     : tomorrow      # 
+widgets     : []            # {mathjax, quiz, bootstrap}
+mode        : standalone # {standalone, draft}
+---
+
+## The Problem of Data Visualization
+
+- Data use is increasing rapidly within the education space
+- Policymakers are under increasing pressure to use data to inform decisions, justify funding, and guide practice
+- **But,** policymakers are often **not** statisticians, researchers, or quants
+- **Data visualization** is a way to bridge this gap
+- Proper data visualization will bring the data to the audience in a way they can understand quickly and use to inform decisions
+
+---
+
+## Follow Along
+
+### http://jknowles.github.com/Jan2013SDPtalk
+
+
+--- &twocol w1:50% w2:50%
+
+## What is dataviz?
+
+***left
+### Dataviz is...
+>- An exploratory tool for understanding datasets
+>- A communication tool for framing decisions and depicting problems
+>- A way to showcase 
+>- A better way to present results of analyses
